1f6691f523b49ae0b3e55e79b5a4492b9f45b35b
Jhon Gomez Marín <gmezjhon@gmail.com>
2020-05-22 17:54:07 -0500

    Creacion del primer servicio

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+* Servicios
+*/
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+  
+  //Variables
+  heroes:Heroe[] = [];
+  
+  //Constructor
+  constructor( private _heroesService:HeroesService) { 
+    
+  }
 
   ngOnInit(): void {
+
+    //Iniciar variable con data
+    this.heroes = this._heroesService.getHeroes();
+    console.log( this.heroes );
+
   }
 
 }
